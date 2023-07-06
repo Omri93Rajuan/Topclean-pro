39e75324b63b39e7a58057786d1a9cededabec69
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2023-07-06 04:13:25 +0300

    first commit

---
+++ client/src/cards/models/types/cardType.js
@@ -0,0 +1,18 @@
+import { shape, string, number, arrayOf, oneOfType } from "prop-types";
+import addressType from "./addressType";
+import imageType from "./imageType";
+
+const cardType = shape({
+  _id: string,
+  company: string.isRequired,
+  title: string.isRequired,
+  role: string.isRequired,
+  description: string.isRequired,
+  phone: string.isRequired,
+  likes: arrayOf(string).isRequired,
+  email: string.isRequired,
+  user_id: string.isRequired,
+  createdAt: string.isRequired,
+});
+
+export default cardType;
